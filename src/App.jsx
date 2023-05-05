22dbf55dc283b91b55b0eedd2bf9a46fa5ac730f
Maxim Gao <maxim@gaofamily.org>
2023-05-05 15:07:20 -0500

    Publishing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import MainContent from "./components/MainContent"
+ 
+export default function App() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <MainContent />
+      </div>
+    </>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className = "navBar">
+            <img className = "nav--logo" src = "../../external/React-icon.png"/>
+            <h3 className = "nav--logo_text">ReactFacts</h3>
+            <h4 className = "nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
